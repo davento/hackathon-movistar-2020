e29eec1711b0cdd0f399bdcc2fc65759fe56de86
davento <daniela.vento@utec.edu.pe>
2020-11-08 16:40:10 -0500

    a

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 import recognition
-import os
 
-to_detect = []
-i = 1
-
-for file in os.listdir('img/fachada'):
-    to_detect = [{"_file": "img/fachada/"+str(file), "noresize": True}]
-    recognition.recognition_facade(to_detect, i)
-    i += 1
-# to_detect = [{"_file": "img/CINTILLO_030.jpg", "noresize": True}]
-# print(to_detect)
+recognition.image_comformity()
