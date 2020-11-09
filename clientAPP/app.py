4e5693ac71a58c59f1f6f6f3d9e8c01ced7f22ba
warleon <anthony.aguilar@utec.edu.pe>
2020-11-09 18:15:13 -0500

    funciona

--- clientAPP/app.py
+++ clientAPP/app.py
@@ -12,27 +12,29 @@ def index():
 
 @app.route('/request_codes', methods=['POST'])
 def codes():
-    APIaddress = "http://127.0.0.1:5000/v1/validate?"
+    APIaddress = 'http://127.0.0.1:5000/v1/validate?'
     try:
         codigo = request.get_json()['codigo']
+        print(codigo)
         #integrar con hAckPI
         ADDRESS= APIaddress + "code=" + codigo
+        print(ADDRESS)
         resp =  requests.get(ADDRESS).json()
         print(resp)
-        resp = json.load(resp)
-        print(resp)
+    except Exception as e:
+        print(e)
 
-
+    finally:
         if(resp["Error"]=="All ok" and resp["status"]):
             respuesta = 'El formulario numero '+ codigo + ' es conforme'
         else:
             respuesta ='El formulario numero '+ codigo + 'no es conforme'
+
+        respuesta += "|Nombre:\n\t" + resp["Name"] + "|Descripcion:\n\t" + resp["Description"]
         
         return jsonify({
             'respuesta': respuesta
         })
-    except Exception as e:
-        print('Ocurrio un error')
 
 
 if __name__ == '__main__':
