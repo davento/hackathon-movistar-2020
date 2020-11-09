f7f5c2bf258a1806fd6f2d3e4ba9c208893ee2f5
CEduardoSQUTEC <eduardo.castro@utec.edu.pe>
2020-11-09 10:57:13 -0500

    encrytor in progress

--- encryptor.py
+++ encryptor.py
@@ -13,51 +13,51 @@ def padFile(file):
         file = file + b'0'
     return file
 
-def encryptString(message, password):
-    password = password.encode()
-    key = hashlib.sha256(password).digest()
+def cipherObject(password):
+    key = password.encode()
+    key = hashlib.sha256(key).digest()
     mode = AES.MODE_CBC
-    paddedMessage = padMessage(message)
     cipher = AES.new(key, mode, IV)
+    return cipher
+
+def encryptString(message, password):
+    paddedMessage = padMessage(message)
+    cipher = cipherObject(password)
     encryptMessage = cipher.encrypt(paddedMessage)
     return encryptMessage
 
-def decryptString(message, password):
-    password = password.encode()
-    key = hashlib.sha256(password).digest()
-    mode = AES.MODE_CBC
-    cipher = AES.new(key, mode, IV)
-    decryptMessage = cipher.decrypt(message)
-    decryptMessage = decryptMessage.rstrip().decode()
-    return decryptMessage
+# def decryptString(message, password):
+#     password = password.encode()
+#     key = hashlib.sha256(password).digest()
+#     mode = AES.MODE_CBC
+#     cipher = AES.new(key, mode, IV)
+#     decryptMessage = cipher.decrypt(message)
+#     decryptMessage = decryptMessage.rstrip().decode()
+#     return decryptMessage
 
-def encryptFile(file, password):
-    key = password.encode()
-    key = hashlib.sha256(key).digest()
-    mode = AES.MODE_CBC
-    cipher = AES.new(key, mode, IV)
-    with open(file, 'rb') as f:
-        originalFile = f.read()
-    padded_file = pad_message(originalFile)
-    encryted_message = cipher.encrypt(padded_file)
-    return encryptedMessage
+# def encryptFile(file, password):
+#     key = password.encode()
+#     key = hashlib.sha256(key).digest()
+#     mode = AES.MODE_CBC
+#     cipher = AES.new(key, mode, IV)
+#     with open(file, 'rb') as f:
+#         originalFile = f.read()
+#     padded_file = pad_message(originalFile)
+#     encryted_message = cipher.encrypt(padded_file)
+#     return encryptedMessage
 
-def decryptFile(file, password):
-    key = password.encode()
-    key = hashlib.sha256(key).digest()
-    mode = AES.MODE_CBC
-    cipher = AES.new(key, mode, IV)
-    with open(file, 'rb') as f:
-        encryptedFile = f.read()
-    decryptedFile = cipher.decrypt(encryptFile)
-
-    
-
-
+# def decryptFile(file, password):
+#     key = password.encode()
+#     key = hashlib.sha256(key).digest()
+#     mode = AES.MODE_CBC
+#     cipher = AES.new(key, mode, IV)
+#     with open(file, 'rb') as f:
+#         encryptedFile = f.read()
+#     decryptedFile = cipher.decrypt(encryptFile)
 
 message = "Abril Vento"
 password = "secret"
 encryptedMessage = encryptString(message, password)
 print(encryptedMessage)
-decryptedMessage = decryptString(encryptedMessage, password)
-print(decryptedMessage)
+# decryptedMessage = decryptString(encryptedMessage, password)
+# print(decryptedMessage)
